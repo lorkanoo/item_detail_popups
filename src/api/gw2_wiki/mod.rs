92dcc3900067a882ca9ee05f002fcaa0f6897aea
lorkanoo <175503300+lorkanoo@users.noreply.github.com>
2024-12-24 14:51:40 +0100

    0.0.8 Alpha release

--- src/api/gw2_wiki/mod.rs
+++ src/api/gw2_wiki/mod.rs
@@ -82,7 +82,11 @@ pub fn prepare_href_popup(href: &String, title: String) -> Popup {
 }
 
 fn prepare_popup(href: &str, title: String) -> Popup {
-    let basic_data = BasicData { title: title.clone(), href: href.to_owned(), ..Default::default() };
+    let basic_data = BasicData {
+        title: title.clone(),
+        href: href.to_owned(),
+        ..Default::default()
+    };
     Popup::new(basic_data)
 }
 
